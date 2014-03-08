7b8253053cef21a3817f69606bc54738e1f81f9b
Dare Ryan <dareryan@me.com>
2014-03-07 19:41:31 -0500

    Add storyboard and main tableViewController. Install FontAwesome cocoaPod. Set custom image for settings barButtonItem.

---
+++ QuidProQueue/QuidProQueue/QueueTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QueueTableViewController.h
+//  
+//
+//  Created by Dare Ryan on 3/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueueTableViewController : UITableViewController
+
+@end
